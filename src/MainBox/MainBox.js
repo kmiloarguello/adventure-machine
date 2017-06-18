89ae06ddb3134f6b02c11f3bc883d1c6b35fd8c8
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-06-18 09:28:09 -0500

    Preventin load more than six AudioContext

--- src/MainBox/MainBox.js
+++ src/MainBox/MainBox.js
@@ -1,15 +1,121 @@
 import React, { Component } from 'react'
 import {Row} from 'react-materialize'
 import SingleBox from './SingleBox.js'
+import Sound from '../sounds/01.mp3'
+
+
+function BufferLoader(context, urlList, callback) {
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = [];
+  this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  var loader = this;
+
+  request.onload = function() {
+    loader.context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        if (!buffer) {
+          alert('error decoding file data: ' + url);
+          return;
+        }
+        loader.bufferList[index] = buffer;
+        if (++loader.loadCount === loader.urlList.length)
+          loader.onload(loader.bufferList);
+      },
+      function(error) {
+        console.error('decodeAudioData error', error);
+      }
+    );
+  }
+
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
+
+  request.send();
+}
+
+BufferLoader.prototype.load = function() {
+  for (var i = 0; i < this.urlList.length; ++i)
+  this.loadBuffer(this.urlList[i], i);
+}
+
+let context, bufferLoader;
+
+function init() {
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  context = new AudioContext();
+
+  bufferLoader = new BufferLoader(
+    context,
+    [
+      Sound
+    ],
+    finishedLoading
+    );
+
+  bufferLoader.load();
+}
+
+function finishedLoading() {
+}
+
+function playSound(buffer, time){
+  let source = context.createBufferSource();
+  source.buffer = buffer;
+  source.connect(context.destination);
+  source.start(time);  
+}
+
+function startPlayingRhythm(bufferList) {
+  let beat = bufferList[0];
+  let startTime = context.currentTime + 0.100;
+  let tempo = 150;
+  let quarterNoteTime = 60 / tempo;
+
+  for (var i = 0; i < 16; i++) {
+      playSound(beat, startTime + i*quarterNoteTime);
+  };
+
+}
+
 
 export default class MainBox extends Component {
+  constructor(props){
+      super(props)
+      this.playSound = this.playSound.bind(this)
+  }
+  componentDidMount(){
+      init()
+  }
+  playSound(){
+      startPlayingRhythm(bufferLoader.bufferList)
+  }  
   render() {
     return (
       <div className="container">
         <Row>
           <div className='contenedor valign-wrapper'>
             <ul>
-              <SingleBox />
+              <SingleBox onClick={this.playSound}/>
+              <SingleBox onClick={this.playSound}/>
+              <SingleBox onClick={this.playSound}/>
+              <SingleBox onClick={this.playSound}/>
+              <SingleBox onClick={this.playSound}/>
+              <SingleBox onClick={this.playSound}/>
+              <SingleBox onClick={this.playSound}/>
+              <SingleBox onClick={this.playSound}/>
+              <SingleBox onClick={this.playSound}/>
+
             </ul>
           </div>
         </Row>
